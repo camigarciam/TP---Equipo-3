280d26b1f4e338dfce8f9ce40842a9aaa97f4da5
camigarciam <camizoegm@gmail.com>
2024-08-26 20:43:33 -0300

    modularizacion

--- code_1.py
+++ code_1.py
@@ -1,7 +1,35 @@
-usuario=input("Ingrese su nombre de usuario: ")
 
+import random
+
+
+##login 
 while len(usuario)<6:
     print("El nombre de usuario debe contener al menos 6 caracteres")
     usuario=input("Ingrese su nombre de usuario: ")
 print("El nombre de usuario es correcto")
 
+ 
+#2. mostrar los titulos de las peliculas 
+def Mostrarpelis():
+   pass
+#3. elegir una peli y mostrar detalles 
+def Elegirpeli(numpeli,peli):
+    pass
+#4. mostrar disponibilidad y seleccionar 
+def Disponibilidad(numpeli,dispo):
+    pass
+#5. sacar la seleccion 
+def Sacar(numpeli):
+    pass
+#6. recomendacion de una pelicula por si no sabes qué elegir! 
+def Recomendacion(num,peli):
+
+    pass
+#7. finalizar
+def Finalizar():
+    pass
+
+
+#programa principal
+
+usuario=input("Ingrese su nombre de usuario: ")
