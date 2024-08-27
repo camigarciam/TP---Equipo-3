e1b3d22e653ac485054d85a9bbd7c8bc0519f9d2
camigarciam <camizoegm@gmail.com>
2024-08-27 08:26:40 -0300

    validar contraseña

--- code_1.py
+++ code_1.py
@@ -3,12 +3,25 @@ import random
 
 
 ##login 
-while len(usuario)<6:
-    print("El nombre de usuario debe contener al menos 6 caracteres")
-    usuario=input("Ingrese su nombre de usuario: ")
-print("El nombre de usuario es correcto")
+def validarusuario(usuario):
+        while len(usuario)<6:
+            print("El nombre de usuario debe contener al menos 6 caracteres")
+            usuario=input("Ingrese su nombre de usuario: ")
+        print("El nombre de usuario es válido")
+        return usuario
 
- 
+def validarcontraseña(nuevacontra):
+    while len(nuevacontra) <8:
+        nuevacontra=input("Contraseña invalida, ingrese la contraseña nuevamente: ")
+    print("Contraseña válida")
+    contraseña=input("Confirme su contraseña: ")
+    while contraseña != nuevacontra:
+        contraseña=input("Las contraseñas no coinciden, ingrese la contraseña nuevamente: ")
+    else:
+        print("Contraseña confirmada")
+    return contraseña
+
+
 #2. mostrar los titulos de las peliculas 
 def Mostrarpelis():
    pass
@@ -32,4 +45,7 @@ def Finalizar():
 
 #programa principal
 
-usuario=input("Ingrese su nombre de usuario: ")
+usuario=input("Cree su nombre de usuario: ")
+usuario=validarusuario(usuario)
+nuevacontra=input("Cree su contraseña, debe contener al menos 8 caracteres: ")
+contraseña=validarcontraseña(nuevacontra)
