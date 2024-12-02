980926ecbdfb518c6d32af1f800f8cbee8d4e0fc
nicolasalgranti <nicolasalgranti@gmail.com>
2024-12-02 12:40:47 -0300

    test encontrar.1

--- test_encontrar.py
+++ test_encontrar.py
@@ -4,16 +4,16 @@ from code_1 import encontrar_usuario
 
 def test_usuario_encontrado():
     usuarios = [
-        {"nombreUsuario": "user1", "edad": 22},
-        {"nombreUsuario": "user2", "edad": 69},
+        {"nombreUsuario": "user1", "contrasena": "buenastardesxd12"},
+        {"nombreUsuario": "user2", "contrasena": "contraseniadificil"},
     ]
     resultado = encontrar_usuario("user1", usuarios)
-    assert resultado == {"nombreUsuario": "user1", "edad": 25}
+    assert resultado == {"nombreUsuario": "user1", "contrasena": "buenastardesxd12"}
 
 def test_usuario_no_encontrado():
     usuarios = [
-        {"nombreUsuario": "user1", "edad": 22},
-        {"nombreUsuario": "user2", "edad": 69},
+        {"nombreUsuario": "user1", "contrasena": "buenastardesxd12"},
+        {"nombreUsuario": "user2", "contrasena": "contraseniadificil"},
     ]
     resultado = encontrar_usuario("user3", usuarios)
     assert resultado is None
